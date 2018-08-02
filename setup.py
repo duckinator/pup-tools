d2dba099242aa24e7627e87c121ab826958c517e
Ellen Marie Dash <me@duckie.co>
2018-08-02 14:05:56 -0400

    ????

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup
 setup(
     entry_points={
         'console_scripts': [
-            'emanate=emanate.cli:main',
+            'pup.xpath=pup.xpath:_main',
+            'pup.http=pup.http:_main',
         ],
         'distutils.commands': [
             'release = distutils_twine:release',
