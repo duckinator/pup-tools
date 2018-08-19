6f2064450bf27fc5bfbb09aeb01cf66ba4474060
Ellen Marie Dash <me@duckie.co>
2018-08-19 02:00:45 -0400

    Move everything from setup.py to setup.cfg.

--- setup.py
+++ setup.py
@@ -2,15 +2,4 @@
 
 from setuptools import setup
 
-setup(
-    entry_points={
-        'console_scripts': [
-            'pup.xpath=pup.xpath:_main',
-            'pup.http=pup.http:_main',
-            'pup.py=pup.py:main',
-        ],
-        'distutils.commands': [
-            'release = distutils_twine:release',
-        ],
-    },
-)
+setup()
